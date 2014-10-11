94cf5878b47098e41ac12401f744395f975ef078
Jim Cowart <jim@ifandelse.com>
2014-10-11 10:12:17 -0400

    Apply esformatter source cleanup (i.e. - whitespace commit)

--- src/postal.xframe.js
+++ src/postal.xframe.js
@@ -1,22 +1,22 @@
-(function(root, factory) {
-    if (typeof module === "object" && module.exports) {
-        // Node, or CommonJS-Like environments
-        module.exports = function(postalFed) {
-            return factory(require("lodash"), postalFed);
-        };
-    } else if (typeof define === "function" && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(["lodash", "postal.federation"], function(_, postal) {
-            return factory(_, postal, root);
-        });
-    } else {
-        // Browser globals
-        root.postal = factory(root._, root.postal, root);
-    }
-}(this, function(_, postal, global, undefined) {
+(function( root, factory ) {
+	if ( typeof define === "function" && define.amd ) {
+		// AMD. Register as an anonymous module.
+		define( [ "lodash", "postal.federation" ], function( _, postal ) {
+			return factory( _, postal, root );
+		} );
+	} else if ( typeof module === "object" && module.exports ) {
+		// Node, or CommonJS-Like environments
+		module.exports = function( postalFed ) {
+			return factory( require( "lodash" ), postalFed );
+		};
+	} else {
+		// Browser globals
+		root.postal = factory( root._, root.postal, root );
+	}
+}( this, function( _, postal, global, undefined ) {
 
-    //import("xframe.js");
+	//import("xframe.js");
 
-    return postal;
+	return postal;
 
-}));
+} ));
